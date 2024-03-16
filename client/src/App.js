baabb00f76b3f922526f7c1e7e45193e96a356ed
parth adhikari <parthadhikari1812@gmail.com>
2024-03-16 17:33:29 +0530

    added state

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import ChatSpace from "./components/ChatSpace";
 import Navbar from "./components/Navbar";
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  //State to hold the Upload-File name
+  const [uploadedFile, setUploadedFile] = useState(null);
   return (
     <div>
-      <Navbar />
-      <ChatSpace />
+      <Navbar setUploadedFile={setUploadedFile} />
+      <ChatSpace uploadedFile={uploadedFile} />
+      <Toaster />
     </div>
   );
 }
